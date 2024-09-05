03ec0c35b48ea441a4eb528a3a54d779d863c1d5
nicole <nicole_tan_yitong@s2021.ssts.edu.sg>
2024-09-05 23:03:18 +0800

    added tabsize and edit lyrics

--- guitar-tabs/src/components/CreateTab.js
+++ guitar-tabs/src/components/CreateTab.js
@@ -1,28 +1,41 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import TabView from './TabView';
 import { Tab } from '../Objects';
-import { FaLessThan, FaCheck } from "react-icons/fa6";
+import { FaLessThan, FaCheck, FaPlus } from "react-icons/fa6";
 import { Link } from 'react-router-dom';
 
 export default function CreateTab() {
   const [title, setTitle] = useState("");
   const [artist, setArtist] = useState("");
+  const tabSize = 10
 
   // e.g. 4/4 beats
   // notes are stored as 1--0|----|----|----|----|----. the | indicates a new string, - indicates no note is played. numbers correspond to the fret number
   // the dash used is — (the 1 em dash)
-  const [tabs, setTabs] = useState([
-    new Tab("Lyric 1", "1--0|1---|1---|--3-|-2--|--1-"),
-  ]);
+  const defaultTab = new Tab(
+        "",
+        Array(6)
+          .fill(["-".repeat(tabSize * 4)])
+          .join("|")
+      )
+  const [tabs, setTabs] = useState([defaultTab]);
+  // 1--0|1---|1---|--3-|-2--|--1-
+
+  const addTab = () => {
+    setTabs((t) => [
+      ...t,
+      defaultTab
+    ]);
+  };
 
   return (
     <div id="create-view">
-      <div id='create-top-bar'>
-        <Link to="/" id="back-btn" className='button'>
+      <div id="create-top-bar">
+        <Link to="/" id="back-btn" className="button">
           <FaLessThan />
           <p>Back</p>
         </Link>
-        <Link to="/" id="done-btn" className='button'>
+        <Link to="/" id="done-btn" className="button">
           <FaCheck />
           <p>Done</p>
         </Link>
@@ -52,13 +65,20 @@ export default function CreateTab() {
           />
         </div>
       </div>
+      <p>Time Signature: 4/4</p>
+      <p>Number of bars per row: 10</p>
       <br />
       <hr />
       <div id="tab-editor">
         {tabs.map((item, id) => {
-          return <TabView key={id} line={item} />;
+          return <TabView key={id} line={item} tabSize={tabSize} />;
         })}
       </div>
+      <br />
+      <button id="add-tabs-btn" className="button" onClick={addTab}>
+        <FaPlus />
+        <p>Add more tabs</p>
+      </button>
     </div>
   );
 }
--- guitar-tabs/src/components/TabView.js
+++ guitar-tabs/src/components/TabView.js
@@ -1,69 +1,98 @@
 import React, { useEffect, useState } from 'react'
+import { FaPen } from "react-icons/fa";
 
 export default function TabView({ line }) {
   // each tab line has 10 notes.
   // e.g. of notes: "1--0|--1-|--1-|--3-|--2-|-1--"
 
   const STRINGS = ["E", "B", "G", "D", "A", "E"];
-  const [edit, setEdit] = useState(null)
-  const [noteInput, setNoteInput] = useState("")
   const [stringNotes, setStringNotes] = useState(line.notes.split("|"));
   
+  const [editNote, setEditNote] = useState(null);
+  const [noteInput, setNoteInput] = useState("");
   
+  const [editLyric, setEditLyric] = useState(null)
+  const [lyricInput, setLyricInput] = useState(line.lyric)
+
   const changeNote = (ev) => {
     // change/add note here
     // add an input box at this place
-    if (edit == null) {
+    if (editNote == null) {
       let index = ev.target.getAttribute("index");
-      setEdit(index);
+      setEditNote(index);
       // if this already had a note, change it to the value of the input
-      console.log(stringNotes[index.split("|")[0]][index.split("|")[1]])
       if (stringNotes[index.split("|")[0]][index.split("|")[1]] != "-") {
         setNoteInput(stringNotes[index.split("|")[0]][index.split("|")[1]]);
       }
     }
-  }
+  };
 
   const confirmNote = (e) => {
     e.preventDefault();
-    const row = edit.split("|")[0]
-    const col = edit.split("|")[1]
+    const row = editNote.split("|")[0];
+    const col = editNote.split("|")[1];
 
     const updatedNotes = stringNotes.map((rowArray, rowIndex) =>
       rowIndex == row
-        ? rowArray.split("").map((value, colIndex) =>
-            colIndex == col ? noteInput : value
-          ).join("")
+        ? rowArray
+            .split("")
+            .map((value, colIndex) => (colIndex == col ? noteInput : value))
+            .join("")
         : rowArray
     );
 
     // Update the state with the new matrix
     setStringNotes(updatedNotes);
-    setEdit(null)
-    setNoteInput("")
+    setEditNote(null);
+    setNoteInput("");
+  };
+
+  const submitLyric = (e) => {
+    e.preventDefault()
+    setEditLyric(false)
   }
-
-  useEffect(() => {
-    console.log(edit)
-  }, [edit])
-
 
   return (
     <div>
       {/* Display Lyric */}
-      <p>{line.lyric}</p>
+      {lyricInput == "" || editLyric ? (
+        <form onSubmit={(e) =>submitLyric(e)}>
+          <input
+            className="lyric-input"
+            type="text"
+            value={lyricInput}
+            placeholder={"Enter lyric..."}
+            onChange={(ev) => {setLyricInput(ev.target.value); !editLyric && setEditLyric(true)}}
+          />
+        </form>
+      ) : (
+        <div className="lyric-wrapper">
+          <p>{lyricInput}</p>
+          <div className="edit-btn" onClick={() => setEditLyric(true)}>
+            <FaPen className="icon" />
+            <p>Edit</p>
+          </div>
+        </div>
+      )}
       {/* Display Tab */}
       {STRINGS.map((letter, id) => {
         const notes = stringNotes[id].split("");
-        console.log(notes);
         return (
           <div key={id} className="string-row">
             <p className="string-letter">{letter}</p>
             <form onSubmit={confirmNote}>
-              <p className="notes" onClick={(ev) => changeNote(ev)} >
+              <p className="notes" onClick={(ev) => changeNote(ev)}>
                 {notes.map((note, k) => {
-                  return edit == id + "|" + k ? (
-                    <input onSubmit={confirmNote} key={k} className='note-input' type="text" maxLength={1} onChange={(ev) => setNoteInput(ev.target.value)} value={noteInput} />
+                  return editNote == id + "|" + k ? (
+                    <input
+                      onSubmit={confirmNote}
+                      key={k}
+                      className="note-input"
+                      type="text"
+                      maxLength={1}
+                      onChange={(ev) => setNoteInput(ev.target.value)}
+                      value={noteInput}
+                    />
                   ) : (
                     // if it is 0, then use e has the letter to differentiate between first and last string
                     <span key={k} index={id + "|" + k}>
